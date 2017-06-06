1a562f00c8e82f6bace410ef0ad2a937e80f254b
ganeshp-c <ganeshp@surveymonkey.com>
2017-06-06 23:24:03 +0530

    adding current by robo

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -11,7 +11,8 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
+        
+        $this->assertEquals(1,0);
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
